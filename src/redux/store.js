4afc782426b1a7e9a27a3be648fa2671f0f0ea74
Sergey Kuran <kuran.serg@gmail.com>
2023-07-24 22:49:03 +0300

    Update store, add redusers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './reducer';
+import { createStore } from '@reduxjs/toolkit';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
-export const store = configureStore({
-  reducer: rootReducer,
-});
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'contactList/contactId':
+      return {
+        ...state.contacts.filter(el => el.id !== action.payload),
+      };
+
+    case 'app/contactsArray':
+      return { ...state };
+
+    default:
+      return state;
+  }
+};
+
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
+
+export const contactId = value => {
+  return {
+    type: 'contactList/contactId',
+    payload: value,
+  };
+};
+
+export const contactsArray = array => {
+  return {
+    type: 'app/contactsArray',
+    payload: array,
+  };
+};
