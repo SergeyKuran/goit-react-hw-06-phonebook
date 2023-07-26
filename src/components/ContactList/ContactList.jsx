2f66f694f4af69d23f3d45ce76b02d88ad8ab98e
Sergey Kuran <kuran.serg@gmail.com>
2023-07-26 09:00:22 +0300

    Update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,27 +3,32 @@ import PropTypes from 'prop-types';
 
 //  Імпорт стилів
 import css from '../ContactList/ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContacts, getContacts } from 'redux/contactSlice';
+// eslint-disable-next-line no-unused-vars
+import { useMemo } from 'react';
 
 // Компонеет для рендеру списку контактів
-const ContactList = ({ contacts, filter, deleteObject }) => {
+const ContactList = () => {
+  const { contacts } = useSelector(getContacts);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <ul className={css.ul}>
-        {contacts
-          .filter(el => el.name.toLowerCase().includes(filter.toLowerCase()))
-          .map(({ id, name, number }) => (
-            <li key={id}>
-              {name}: {number}
-              <button
-                type="button"
-                onClick={() => {
-                  deleteObject(id);
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
+        {contacts.map(({ id, name, number }) => (
+          <li key={id}>
+            {name}: {number}
+            <button
+              type="button"
+              onClick={() => {
+                dispatch(deleteContacts(id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
@@ -32,7 +37,6 @@ const ContactList = ({ contacts, filter, deleteObject }) => {
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object.isRequired),
   filter: PropTypes.string.isRequired,
-  deleteObject: PropTypes.func.isRequired,
 };
 
 export default ContactList;
